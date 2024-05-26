3b496f033f9de8d65e8ec06b05461c2b60d18c38
Adam Stitou <adamkoora27@gmail.com>
2024-05-26 12:58:06 +0100

    main icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import { MoveRight } from "lucide-react";
+import {
+  MoveRight,
+  BarChart4,
+  Building,
+  Clock,
+  ListTodo,
+  Folder,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { MotionDiv } from "@/components/MotionDiv";
 export default function Home() {
@@ -16,23 +23,37 @@ export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary relative">
-        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,primary_40%,#63e_100%)]" />
-        <div className="z-10 w-3/4 h-[500px] items-center justify-center text-sm text-center gap-4 relative overflow-hidden flex flex-col">
-          <div className="overflow-hidden h-11 w-full">
-            <MotionDiv
-              variants={variants}
-              initial="hidden"
-              animate="visible"
-              transition={{
-                duration: 0.1,
-                ease: "easeIn",
-              }}
-              viewport={{ once: false }}
-              className="relative"
-            >
-              <h1 className="text-5xl font text-secondary">Controllad</h1>
-            </MotionDiv>
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,hsl(var(--primary))_40%,#63e_100%)]" />
+        <div className="z-10 w-3/4 h-[500px] items-center justify-center text-sm text-center gap-4 relative flex flex-col">
+          <div className="h-14 w-14 rounded-lg  absolute top-4 left-4 bg-white flex justify-center items-center">
+            <BarChart4 className="h-8 w-8" />
           </div>
+          <div className="h-14 w-14 rounded-lg  absolute bottom-6 left-11 bg-white flex justify-center items-center">
+            <Folder className="h-8 w-8" />
+          </div>
+          <div className="h-14 w-14 rounded-lg  absolute bottom-8 right-12 bg-white flex justify-center items-center">
+            <ListTodo className="h-8 w-8" />
+          </div>
+          <div className="h-14 w-14 rounded-lg  absolute top-11 right-20 bg-white flex justify-center items-center">
+            <Clock className="h-8 w-8" />
+          </div>
+  
+          <MotionDiv
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              duration: 0.1,
+              ease: "easeIn",
+            }}
+            viewport={{ once: false, amount: 0.5 }}
+            className="relative"
+          >
+            <h1 className="text-8xl font text-secondary">
+              Manage{" "}
+              <span className="block text-accent">Anywhere, Evrywhere</span>
+            </h1>
+          </MotionDiv>
 
           <div>
             <p className="text-sm text-gray-400">
